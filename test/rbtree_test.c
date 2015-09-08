4b219eb44edbcf8bdcc08006ec3748f4fe3a5665
zuodengpeng <cqzuodp@163.com>
2015-09-08 21:47:49 +0800

    FIX: rbtree中加入ez_malloc的测试例子

--- test/rbtree_test.c
+++ test/rbtree_test.c
@@ -6,28 +6,27 @@
 #define NODES 100
 
 #define cast_to_type_ptr(ptr, type)     EZ_CONTAINER_OF(ptr, type, rb_node)
-#define rb_entry(type_ptr)          (&((type_ptr)->rb_node))
-#define RB_TREE_NODE                ezRBTreeNode rb_node
+#define rb_node_addr(type_ptr)         ((ezRBTreeNode*)(&(type_ptr->rb_node)))
 
-typedef struct student {
-    RB_TREE_NODE;
+typedef struct test_t {
+    ezRBTreeNode rb_node;
     int age;
-} student;
+} test;
 
 static ezRBTree rbtree;
 static ezRBTreeNode sentinel;
 
-static student *nodes[NODES];
+static test *nodes[NODES];
 
 static int student_compare_proc(ezRBTreeNode *new_node, ezRBTreeNode *exists_node) {
-    student *n = cast_to_type_ptr(new_node, student);
-    student *o = cast_to_type_ptr(exists_node, student);
+    test *n = cast_to_type_ptr(new_node, test);
+    test *o = cast_to_type_ptr(exists_node, test);
     return n->age == o->age ? 0 : (n->age > o->age ? 1 : -1);
 }
 
 static int student_find_proc(ezRBTreeNode *node, void *find_args) {
     int age = *((int *) find_args);
-    student *o = cast_to_type_ptr(node, student);
+    test *o = cast_to_type_ptr(node, test);
     return o->age == age ? 0 : (o->age > age ? 1 : -1);
 }
 
@@ -36,26 +35,26 @@ int main(int argc, char **argv) {
     rbtree_init(&rbtree, &sentinel, student_compare_proc);
 
     for (int i = 0; i < NODES; ++i) {
-        nodes[i] = (student *) ez_malloc(sizeof(student));
+        nodes[i] = (test *) ez_malloc(sizeof(test));
         nodes[i]->age = i;
     }
 
     for (int i = 0; i < NODES; ++i) {
-        rbtree_insert(&rbtree, rb_entry(nodes[i]));
+        rbtree_insert(&rbtree, rb_node_addr(nodes[i]));
     }
     int find_age = 100;
     node = rbtree_find_node(&rbtree, student_find_proc, &find_age);
     if (node != NULL) {
-        fprintf(stdout, "error:::>find student 100 :>!\n");
+        fprintf(stdout, "error:::>find test_t 100 :>!\n");
     }
     find_age = 50;
     node = rbtree_find_node(&rbtree, student_find_proc, &find_age);
-    if (node == NULL || cast_to_type_ptr(node, student)->age != find_age) {
-        fprintf(stdout, "error::>not find student 50 :>!\n");
+    if (node == NULL || cast_to_type_ptr(node, test)->age != find_age) {
+        fprintf(stdout, "error::>not find test_t 50 :>!\n");
     }
     node = rbtree_min_node(&rbtree);
     while (node != NULL) {
-        fprintf(stdout, "delete min student.age = %d\n", cast_to_type_ptr(node, student)->age);
+        fprintf(stdout, "delete min test_t.age = %d\n", cast_to_type_ptr(node, test)->age);
         rbtree_delete(&rbtree, node);
         node = rbtree_min_node(&rbtree);
     }
