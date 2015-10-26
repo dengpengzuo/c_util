e5f818e974aefda215a0c25e7fc05cee31d5fe1a
zuodengpeng <cqzuodp@163.com>
2015-10-26 22:07:02 +0800

    FIX: 加入现在比较流行的Hash算法.

--- include/ez_hash.h
+++ include/ez_hash.h
@@ -29,4 +29,15 @@ uint32_t jenkins_hash(const char *key, size_t length);
  */
 uint64_t fnv_hash(const char *key, size_t length);
 
+/**
+ * google code's provider MurmurHash3
+ */
+uint32_t MurmurHash3_x86_32(const void *key, int len, uint32_t seed);
+
+// 128 分成 4个32
+void MurmurHash3_x86_128(const void *key, int len, uint32_t seed, uint32_t *out);
+
+// 128 分成 2个64
+void MurmurHash3_x64_128(const void *key, int len, uint32_t seed, uint64_t *out);
+
 #endif				/* __EZ_HASH_H */
