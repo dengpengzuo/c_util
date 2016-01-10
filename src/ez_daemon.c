0493007a9d48a3dc9cc49775182b112d3e7d4925
zuodengpeng <cqzuodp@163.com>
2016-01-11 07:38:05 +0800

    FIX: 深入了解下linux.fork的控制，加上2nd fork.

--- src/ez_daemon.c
+++ src/ez_daemon.c
@@ -2,33 +2,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <sys/stat.h>
 #include "ez_daemon.h"
 
-// cur process daemon
 int ez_daemonize(int nochdir, int noclose)
 {
 	int fd;
+	int pid;
 
-	switch (fork()) {
+	pid = fork();
+	switch (pid) {
 	case -1:
 		return (-1);
 	case 0:
 		break;
-	default:
-		// parent process exit,but not close parent open resources.
-		// let childrent safe go....
+        default:
+		// parent process exit, but not close parent open resources.
+		// let childrent safe go ....
 		_exit(EXIT_SUCCESS);
 	}
 
-	if (setsid() == -1)
+	if (setsid() == -1)     /* become session leader */
 		return (-1);
 
+	if (signal(SIGHUP, SIG_IGN) == SIG_ERR) {
+        perror("child signal(SIGHUP, SIG_IGN) error!");
+		return -1;
+	}
+
+	/* 2nd fork turns child into a non-session leader: cannot acquire terminal */
+	pid = fork();
+	switch (pid) {
+		case -1:
+			return -1;
+		case 0:
+			break;
+		default:
+			/* 1st child terminates */
+			_exit(EXIT_SUCCESS);
+	}
+
+    /* change working directory */
 	if (nochdir == 0) {
 		if (chdir("/") != 0) {
 			perror("chdir");
 			return (-1);
 		}
 	}
+
+    /* clear file mode creation mask */
+    umask(0);
+
+    /* redirect stdin, stdout and stderr to "/dev/null" */
 	// children process set (stdin,stdout,stderr) -> fd
 	if (noclose == 0 && (fd = open("/dev/null", O_RDWR, 0)) != -1) {
 		// close STDIN_FILENO, switch children process STDIN_FILENO file pointer to fd
