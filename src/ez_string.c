11326d812e94bae5e8252c1725e76cf22ecd19d6
dengpeng.zuo <cqzuodp@163.com>
2018-05-01 10:30:09 +0800

    fix: utf8 编码.

--- src/ez_string.c
+++ src/ez_string.c
@@ -1,27 +1,24 @@
-#include <stdio.h>
-#include <stdarg.h>
-
 #include "ez_string.h"
 
-char *ez_strncpy(char *dest, char *src, size_t n) {
+char *ez_strncpy(char *dst, char *src, size_t n) {
     if (n == 0) {
-        return dest;
+        return dst;
     }
 
     while (--n) {
-        *dest = *src;
+        *dst = *src;
 
-        if (*dest == '\0') {
-            return dest;
+        if (*dst == '\0') {
+            return dst;
         }
 
-        dest++;
+        dst++;
         src++;
     }
 
-    *dest = '\0';
+    *dst = '\0';
 
-    return dest;
+    return dst;
 }
 
 int ez_vsnprintf(char *buf, size_t size, const char *fmt, va_list args) {
@@ -57,4 +54,70 @@ int ez_snprintf(char *buf, size_t size, const char *fmt, ...) {
     va_end(args);
 
     return i;
+}
+
+/*
+    0000 0000->0000 007F | 0xxxxxxx                             // 7  位有效位.
+    0000 0080->0000 07FF | 110xxxxx 10xxxxxx                    // 11 位有效位.
+    0000 0800->0000 FFFF | 1110xxxx 10xxxxxx 10xxxxxx           // 16 位有效位.
+    0001 0000->0010 FFFF | 11110xxx 10xxxxxx 10xxxxxx 10xxxxxx  // 21 位有效位.  TODO:?? 按理是 0x1f_FFFF
+    ---------------------+-----------------------------------------------------------------------------
+    0020 0000->03ff ffff | 111110xx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx // 26 位有效位.
+    0400 0000->7fff ffff | 1111110x 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx 10xxxxxx // 31 位有效位.
+    ---------------------+-----------------------------------------------------------------------------
+*/
+size_t ez_utf8_encode(u_int8_t *dst, const u_char *src, size_t n) {
+    size_t c = 0;
+    for (size_t i = 0; i < n; ++i) {
+        if (src[i] <= 0x7f) {
+            dst[c++] = src[i++];
+        } else if (src[i] >= 0x80 && src[i] <= 0x07FF) {
+            dst[c++] = (u_int8_t) (0xC0 + ((src[i] >> 6) & 0x1F)); // 高5位 -> 1_1111
+            dst[c++] = (u_int8_t) (0x80 + (src[i] & 0x3F));         // 低6位 -> 11_1111
+        } else if (src[i] >= 0x800 && src[i] <= 0xFFFF) {
+            dst[c++] = (u_int8_t) (0xE0 + ((src[i] >> 12) & 0xF)); // 高4位 -> 1111
+            dst[c++] = (u_int8_t) (0x80 + ((src[i] >> 6) & 0x3F));  // 6位
+            dst[c++] = (u_int8_t) (0x80 + (src[i] & 0x3F));         // 6位
+        } else if (src[i] >= 0x10000 && src[i] <= 0x10FFFF) {
+            dst[c++] = (u_int8_t) (0xF0 + ((src[i] >> 18) & 0x7)); // 高3位 -> 111
+            dst[c++] = (u_int8_t) (0x80 + ((src[i] >> 12) & 0x3F));// 6位
+            dst[c++] = (u_int8_t) (0x80 + ((src[i] >> 6) & 0x3F)); // 6位
+            dst[c++] = (u_int8_t) (0x80 + (src[i] & 0x3F));        // 6位
+        } else {
+            // skip
+        }
+    }
+    return c;
+}
+
+// 取从最高位起,有几位1.
+static inline int left_bits(u_int8_t x) {
+    // 2, 3, 4
+    int bit = 0;
+    while ((x & 0x80) == 0x80) {
+        x = x << 1;
+        ++bit;
+    }
+    return bit;
+}
+
+size_t ez_utf8_decode(u_char *dst, const u_int8_t *src, size_t n) {
+    size_t c = 0;
+    for (size_t i = 0; i < n; ++i) {
+        if (src[i] <= 0x7f) {
+            dst[c++] = (u_char) src[i++];
+        } else {
+            int bit = left_bits(src[i]);
+            if (bit == 2) {
+
+            } else if (bit == 3) {
+
+            } else if (bit == 4) {
+
+            } else {
+                // skip
+            }
+        }
+    }
+    return c;
 }
--- src/ez_string.h
+++ src/ez_string.h
@@ -1,15 +1,24 @@
 #ifndef _EZ_STRING_H
 #define _EZ_STRING_H
 
+#include <sys/types.h>
+#include <sys/time.h>
 #include <unistd.h>
-#include <stdint.h>
 #include <stdarg.h>
+#include <stddef.h>             /* offsetof() */
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-char *ez_strncpy(char *dest, char *src, size_t n);
+char *ez_strncpy(char *dst, char *src, size_t n);
 
 /** 格式化输出函数 snprintf, vsnprintf */
 int ez_snprintf(char *buf, size_t size, const char *fmt, ...);
 
 int ez_vsnprintf(char *buf, size_t size, const char *fmt, va_list args);
+
+size_t ez_utf8_encode(u_int8_t *dst, const u_char *src, size_t n);
+
+size_t ez_utf8_decode(u_char *dst, const u_int8_t *src, size_t n);
 
 #endif /* _EZ_STRING_H */
