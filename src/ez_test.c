bf39072a298bf7ef693e9929c450e730773ad5fc
zuodengpeng <cqzuodp@163.com>
2019-11-09 21:42:44 +0800

    add: test_suit.

--- src/ez_test.c
+++ src/ez_test.c
@@ -2,20 +2,20 @@
 
 #include "ez_test.h"
 
-static list_head_t test_root;
+static list_head_t default_suite;
 
 void init_default_suite() {
-    init_list_head(&test_root);
+    init_list_head(&default_suite);
 }
 
 void suite_add_test(test_suit_t *test) {
     init_list_head(&test->next);
-    list_add(&test_root, &test->next);
+    list_add(&default_suite, &test->next);
 }
 
 void run_default_suite() {
     test_suit_t *p;
-    LIST_FOR(&test_root, ti) {
+    LIST_FOR(&default_suite, ti) {
         p = cast_to_test_suit(ti);
         fprintf(stdout, "==== Test %s \n", p->name);
         p->func();
